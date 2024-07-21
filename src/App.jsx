fefbd0931ec7290544ec8e711326bc424d679d9d
ahmed96-sudo <asyd12855@gmail.com>
2024-07-21 17:57:37 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+import Dashbord from './components/Dashbord';
+import Avoire from './components/Avoire';
+import Bl from './components/Bl';
+import Devis from './components/Devis';
+import Factur from './components/Factur';
+import Login from './components/Login';
+
+
+// https://managementstock.herokuapp.com/
+// http://127.0.0.1:5000/
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashbord />} />
+        <Route path="/avoire" element={<Avoire />} />
+        <Route path="/bl" element={<Bl />} />
+        <Route path="/devis" element={<Devis />} />
+        <Route path="/facture" element={<Factur />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,68 @@
+import '../stylesheets/Login.css';
+import '../fontawesome-free-5.15.3-web/css/fontawesome.css';
+import '../fontawesome-free-5.15.3-web/css/brands.css';
+import '../fontawesome-free-5.15.3-web/css/solid.css';
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+    const navigate = useNavigate();
+    const handleSubmit = (e)=>{
+        const inputuser = document.getElementById("inputuser").value;
+        const inputpass = document.getElementById("inputpassword").value;
+        const logobj = {
+            inputuser: inputuser,
+            inputpass: inputpass
+        };
+        e.preventDefault();
+        console.log(logobj);
+        navigate("/dashboard");
+        /* fetch("https://asyd12855.pythonanywhere.com/login",{
+            method: "POST",
+            body: JSON.stringify({ logobj: logobj }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                navigate("/dashboard");
+            } else {
+                alert("Your password/username isn't correct");
+            }
+        })
+        .catch(error => {
+            console.error(
+                "There has been a problem:",
+                error
+            );
+            alert("There has been a problem with the login");
+        }); */
+    }
+    const handleClick = ()=>{
+        const userps = document.getElementById("inputpassword");
+        if (userps.type == "password") {
+            userps.type = "text";
+        } else {
+            userps.type = "password";
+        }
+    }
+    return (
+        <div className='container'>
+            <div className='form_All'>
+                <div className='head'>
+                    <span id='stspan' className='fas fa-atom'></span>
+                    <h1>KingStock</h1>
+                </div>
+                <form method='POST' onSubmit={handleSubmit}>
+                    <input type="text" className='inputs' id='inputuser' required placeholder='UserName' />
+                    <input type="password" className='inputs' id='inputpassword' required placeholder='Password' />
+                    <label htmlFor="showpass"><input type="checkbox" name='showpass' id='showpass' onClick={handleClick} /> Show Password</label>
+                    <input type="submit" value="Login" id='submit_login' />
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
